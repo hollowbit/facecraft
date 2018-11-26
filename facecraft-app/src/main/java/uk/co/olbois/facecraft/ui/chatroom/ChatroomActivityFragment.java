3d19ccdc169c78a2c37120189645fca1f2241525
alexburroughs <1628499@johnabbottcollege.net>
2018-11-26 13:02:04 -0500

    Started more recycler view

--- facecraft-app/src/main/java/uk/co/olbois/facecraft/ui/chatroom/ChatroomActivityFragment.java
+++ facecraft-app/src/main/java/uk/co/olbois/facecraft/ui/chatroom/ChatroomActivityFragment.java
@@ -9,7 +9,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.List;
+
 import uk.co.olbois.facecraft.R;
+import uk.co.olbois.facecraft.model.message.Message;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -26,6 +29,63 @@ public class ChatroomActivityFragment extends Fragment {
 
 
         return view;
+    }
+
+    private  class MessageViewHolder extends RecyclerView.ViewHolder {
+
+        private final View root;
+        private Message message;
+
+        public MessageViewHolder(@NonNull View itemView) {
+            super(itemView);
+            root = itemView;
+
+            root.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+        }
+
+        public void setMessage(Message message) {
+            this.message = message;
+
+        }
+    }
+
+    private class MessageAdapter extends RecyclerView.Adapter<MessageViewHolder> {
+
+        private List<Message> data;
+
+        public MessageAdapter(List<Message> data) {
+            this.data = data;
+        }
+
+        public List<Message> getData() {
+            return data;
+        }
+
+        @NonNull
+        @Override
+        public MessageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new MessageViewHolder(
+                    LayoutInflater.from(parent.getContext())
+                            .inflate(R.layout.list_item_message, parent, false)
+            );
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MessageViewHolder noteViewHolder, int position) {
+            noteViewHolder.setMessage(data.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return data.size();
+        }
+
+
     }
     
 
