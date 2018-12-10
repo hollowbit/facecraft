39569705ea13b74b24479cda984bd4a9481c8de5
alexburroughs <1628499@johnabbottcollege.net>
2018-12-10 12:58:35 -0500

    Added events and message packets to the bukkit plugin

--- facecraft-plugin/src/main/kotlin/uk/co/olbois/facecraftplugin/events/FacecraftEvents.kt
+++ facecraft-plugin/src/main/kotlin/uk/co/olbois/facecraftplugin/events/FacecraftEvents.kt
@@ -1,2 +1,25 @@
 package uk.co.olbois.facecraftplugin.events
 
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.AsyncPlayerChatEvent
+import uk.co.olbois.facecraftplugin.networking.packet.MessagePacket
+import java.util.*
+
+class FacecraftEvents : Listener {
+
+    @EventHandler
+    fun onPlayerChat(event: AsyncPlayerChatEvent) {
+        var message = event.message
+        var user = event.player.name
+        var type = "game"
+        var date = Date()
+
+        var messagePacket : MessagePacket
+
+        messagePacket = MessagePacket(message, user, type, date)
+
+        // send packet "messagePacket"
+    }
+
+}
--- facecraft-plugin/src/main/kotlin/uk/co/olbois/facecraftplugin/networking/packet/MessagePacket.kt
+++ facecraft-plugin/src/main/kotlin/uk/co/olbois/facecraftplugin/networking/packet/MessagePacket.kt
@@ -1,2 +1,19 @@
 package uk.co.olbois.facecraftplugin.networking.packet
 
+import java.util.*
+
+class MessagePacket () : Packet(PacketType.MESSAGE) {
+
+    private var message : String = ""
+    private var name : String = ""
+    private var gameType : String = ""
+    private var date : Date = Date()
+
+    constructor(message : String, name : String, gameType : String, date : Date) : this() {
+        this.message = message
+        this.name = name
+        this.gameType = gameType
+        this.date = date
+    }
+
+}
